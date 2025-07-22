79a9320cec6d9ba8abb43f2b2a0c26ae7b7e3ee2
arunsinghTL <arun0211.singh@gmail.com>
2025-07-22 09:17:59 +0530

    v1.0.8

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const pwaOptions = {
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest\.json$/, /dynamic-css-manifest\.json$/],
   mode: 'production',
   swSrc: './service-worker.js',  // Custom SW location (root of project),
 };
